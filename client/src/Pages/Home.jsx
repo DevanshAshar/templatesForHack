f0ec0bdfe862a3d88300cd7833a581a35b99c878
Varun Viswanath <varunvis2903@gmail.com>
2023-03-11 11:31:06 +0530

    Map

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useOutletContext } from "react-router-dom";
 import { useState } from "react";
 import MapWithMarker from "../Components/MapWithMarker";
+import MyMapComponent from "../Components/Map/MyMapComponent";
+import MapMarker from "../Components/Map/MapMarker";
 
 export default function Home() {
   const [auth, setAuth] = useOutletContext();
@@ -10,7 +12,7 @@ export default function Home() {
   return (
     <>
       <h1>HOME!!</h1>
-      <MapWithMarker />
+      <MapMarker />
     </>
   );
 }
